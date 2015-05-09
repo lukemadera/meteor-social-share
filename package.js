95df201977131f86cab473c10633c6e3c245126a
Luke Madera <luke.madera@gmail.com>
2015-05-08 18:32:58 -0700

    v1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lukemadera:social-share',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
